6f41f08c3b0569875db0aa386b222230f9899363
Iuliia Barankova <barankovauv@gmail.com>
2022-11-13 00:52:54 +0300

    example slides

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ window.addEventListener('DOMContentLoaded', function() {
 // load all images
 //каждую подпапку импортируем отдельно (иконки просто копируются в конфиге)
 const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg|gif)$/));
-const slider = importAll(require.context('./images/slider', false, /\.(png|jpe?g|svg|gif)$/));
+const exampleSlider = importAll(require.context('./images/example-slider', false, /\.(png|jpe?g|svg|gif)$/));
 const bg = importAll(require.context('./images/bg', false, /\.(png|jpe?g|svg|gif)$/));
 const figureBg = importAll(require.context('./images/figure-bg', false, /\.(png|jpe?g|svg|gif)$/));
 const testimonials = importAll(require.context('./images/testimonials', false, /\.(png|jpe?g|svg|gif)$/));
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -2,11 +2,12 @@ function swiper() {
     var swiper = new Swiper(".example-swiper", {
         slidesPerView: 3,
         spaceBetween: 30,
-        // speed: 500,
-        // autoplay: {
-        //     delay: 2500,
-        //     disableOnInteraction: false,
-        // },
+        loop: true,
+        speed: 500,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
         navigation: {
             nextEl: ".example-swiper-button-next",
             prevEl: ".example-swiper-button-prev",
